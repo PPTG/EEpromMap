3d254834cc5cad851e17707ec0441f4726723f38
PPTG <polskiavatar@gmail.com>
2022-07-05 23:05:20 +0200

    EEpromMap V0.01 04.07.2022

--- EEpromMap.py
+++ EEpromMap.py
@@ -12,6 +12,7 @@ baudrate_select = ('9600','19200','11200','500000','2000000')
 layout = [
     [sg.Canvas(background_color='grey', size=(window_width,window_height),key='canvas')],
     [sg.Text('Port:'),sg.Combo(com_select,default_value=com_select[0], s=(15,22), enable_events=True, readonly=True, k='-com-'),sg.Text('Baud:'),sg.Combo(baudrate_select,default_value=baudrate_select[0], s=(15,22), enable_events=True, readonly=True, k='-baud-')],
+    [sg.Text('Display cell number: '),sg.Checkbox('saved cells', default=True, enable_events=True, key='-cellsave-'),sg.Checkbox('empty cells', default=True, enable_events=True, key='-cellempty-')],
     [sg.Submit('Read EEprom'), sg.Cancel('Exit')]
 ]
 window = sg.Window('Arduino_sketch EEpromMap', layout, finalize=True)
@@ -23,6 +24,7 @@ def make_map():
     eeprom_map.draw(1, "map.txt")
 
 class eeprom_map:
+    cellnumber=3
     size = 20
     margin = size - 1
     licznik = 0
@@ -41,13 +43,17 @@ class eeprom_map:
             wartosc = int(liczba[eeprom_map.licznik])
             if (wartosc == 0):
                 window['canvas'].TKCanvas.create_rectangle(eeprom_map.x, eeprom_map.y,eeprom_map.x + eeprom_map.size,eeprom_map.y + eeprom_map.size, fill="lime")
+                if(eeprom_map.cellnumber == 3 or eeprom_map.cellnumber == 2 ):
+                    window['canvas'].TKCanvas.create_text(eeprom_map.x + 10, eeprom_map.y + 8, fill="black",font="Times 6 italic bold", text=str(eeprom_map.licznik))
                 eeprom_map.x = eeprom_map.x + (eeprom_map.size + 1)
+
 
             if (wartosc != 0):
                 window['canvas'].TKCanvas.create_rectangle(eeprom_map.x, eeprom_map.y,eeprom_map.x + eeprom_map.size,eeprom_map.y + eeprom_map.size, fill="red")
-
-                window['canvas'].TKCanvas.create_text(eeprom_map.x + 10, eeprom_map.y + 8, fill="black", font="Times 6 italic bold",text=str(eeprom_map.licznik) )
+                if (eeprom_map.cellnumber == 3 or eeprom_map.cellnumber == 1):
+                    window['canvas'].TKCanvas.create_text(eeprom_map.x + 10, eeprom_map.y + 8, fill="black", font="Times 6 italic bold",text=str(eeprom_map.licznik) )
                 eeprom_map.x = eeprom_map.x + (eeprom_map.size + 1)
+
             if (eeprom_map.x > eeprom_map.window_width - eeprom_map.size):
                 eeprom_map.y = eeprom_map.y + eeprom_map.size
                 eeprom_map.x = eeprom_map.margin
@@ -56,12 +62,25 @@ class eeprom_map:
 
 while True:
     event, values = window.read()
+    print(event,"val:",values['-cellempty-'])
     if event in (sg.WIN_CLOSED, 'Exit'):
         if os.path.exists("dump.txt"):
             os.remove("dump.txt")
         if os.path.exists("map.txt"):
             os.remove("map.txt")
         break
+    if (event == '-cellempty-' or event == '-cellsave-'):
+        if(values['-cellempty-'] == False):
+            eeprom_map.cellnumber=1
+
+        if (values['-cellsave-'] == False):
+            eeprom_map.cellnumber = 2
+
+        if (values['-cellsave-'] == True and values['-cellempty-'] == True ):
+            eeprom_map.cellnumber = 3
+
+        if (values['-cellsave-'] == False and values['-cellempty-'] == False ):
+            eeprom_map.cellnumber = 0
 
     if event == '-com-':
         selected_com = values['-com-']
